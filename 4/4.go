71d35f9f76373574ce0472012853a34a4f81b35c
icanhcorpse <icanhcorpse@yahoo.com>
2017-07-13 16:03:19 -0400

    Update answer for question 4

--- 4/4.go
+++ 4/4.go
@@ -1,98 +1,98 @@
 package main
 
 import (
-  "fmt"
-  "math"
+    "fmt"
+    "math"
+    "bufio"
+    "os"
+    "strconv"
 )
+	
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
-  var i int
-  
-  fmt.Print("Number of test cases: ")
-  _, err := fmt.Scanf("%d", &i)
-  
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    if i >= 1 && i <= 100 {
-      fmt.Println("You would like to have", i, "test cases")
-
-      var j int = 0
-      var answer[] int
-      for j < i {
-        var A, B, K int
-        fmt.Println("Case", j+1)
-        fmt.Print("First Number: ")
-        fmt.Scanf("%d", &A)
-
-        if ValidateAB(A) {
-          fmt.Print("Second Number: ")
-          fmt.Scanf("%d", &B)
-
-          if ValidateAB(B) {
-            fmt.Print("Third Number: ")
-            fmt.Scanf("%d", &K)
-
-            if ValidateK(K) {
-              answer = append(answer, TotalDivisible(A,B,K))
-              j++ 
-            } else {
-              answer = answer[:0]
-              fmt.Println("The third number must be between 0-10000")
-            } 
-          } else {
-            fmt.Println("The second number must be between 0-10001")
-          }
-        } else {
-          fmt.Println("The second number must be between 0-10001")
-        } 
-      }
-
-      fmt.Print("\nOutput\n")
-
-      if len(answer) > 0 {
-        j = 1
-        for j <= i {
-          fmt.Printf("Case %d: ", j)
-          fmt.Print(answer[j-1], "\n")
-          j++
+    file, _ := os.Open("input.in")
+    scanner := bufio.NewScanner(file)
+    
+    var input[] string
+    var lines[] int
+    
+    for scanner.Scan() {    
+        input = append(input, scanner.Text())
+    }
+    
+    // Convert string in array to int
+    for _, input := range input {
+		j, err := strconv.Atoi(input)
+		if err != nil {
+			panic(err)
+		}
+		lines = append(lines, j)
+	}
+        
+    x, y := 1, 4
+    ytop := lines[0]*3+1
+    
+    var answer[] int
+    
+    for y <= ytop {
+        answer = append(answer, TotalDivisible(lines[x], lines[x+1], lines[y-1]))
+        x = y
+        y += 3
+    }
+    
+    if len(answer) > 0 {
+        j := 1
+        
+        f, err := os.Create("answer.txt")
+        check(err)
+        
+        defer f.Close()
+        
+        var str string
+        for j <= lines[0] {
+            str += fmt.Sprintf("Case %d: %d\n", j, answer[j-1])
+            j++
         }
-      }
-    } else {
-      fmt.Println("Please input number from 1 to 100 only")
-    } 
-  }
+        f.WriteString(str)
+        
+        fmt.Println("Done")
+    }
 }
 
 func IsDivisible(x int, y int) bool {
     if math.Mod(float64(x), float64(y)) == 0 {
-      return true
+        return true
     } else {
-      return false
+        return false
     }
 }
 
 func TotalDivisible(a int, b int, k int) int {
-  var counter int = 0
-  for a <= b {
-    if IsDivisible(a, k) {
-      counter++
+    var counter int = 0
+    for a <= b {
+        if IsDivisible(a, k) {
+            counter++
+        }
+        a++
     }
-    a++
-  }
-  return counter
+    return counter
 }
 
 func ValidateAB(x int) bool {
-  if x >= 1 && x <=10000 {
-    return true
-  }
-  return false
+    if x >= 1 && x <=10000 {
+        return true
+    }
+    return false
 }
 
 func ValidateK(x int) bool {
-  if x >= 1 && x <10000 {
-    return true
-  }
-  return false
+    if x >= 1 && x <10000 {
+        return true
+    }
+    return false
 }
